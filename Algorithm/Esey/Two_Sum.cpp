5ee3ae748055698b4bffb4d9b2a36cbbd4ba9273
jungminna03 <96573337+jungminna03@users.noreply.github.com>
2026-03-17 21:11:04 +0900

    [ADD] two Sum

--- Algorithm/Esey/Two_Sum.cpp
+++ Algorithm/Esey/Two_Sum.cpp
@@ -11,9 +11,33 @@ using namespace std;
 class Solution
 {
     public:
+        vector<int> twoSumPrev(vector<int> &nums, int target)
+        {
+            for (int i = 0; i < nums.size(); i++)
+            {
+                for (int j = i + 1; j < nums.size(); j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return {i, j};
+                }
+            }
+
+            return vector<int>();
+        }
+
         vector<int> twoSum(vector<int> &nums, int target)
         {
-            
+            unordered_map<int, int> map;
+
+            for (int i = 0; i < nums.size(); i++)
+            {
+                auto targetNum = target - nums[i];
+
+                if (map.find(targetNum) != map.end())
+                    return vector<int>{map[targetNum], i};
+
+                map[nums[i]] = i;
+            }
 
             return vector<int>();
         }
@@ -22,13 +46,23 @@ class Solution
 int main()
 {
     Solution s;
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
+    vector<int> nums1 = {2, 7, 11, 15};
+    int target1 = 9;
+    vector<int> nums2 = {3, 2, 4};
+    int target2 = 6;
+    vector<int> nums3 = {3, 3};
+    int target3 = 6;
 
-    auto result = s.twoSum(nums, target);
+    auto result1 = s.twoSum(nums1, target1);
+    auto result2 = s.twoSum(nums2, target2);
+    auto result3 = s.twoSum(nums3, target3);
 
-    for (int i = 0; i < result.size(); i++)
-        cout << result[i] << " ";
+    for (int i = 0; i < result1.size(); i++)
+        cout << result1[i] << " ";
+    for (int i = 0; i < result2.size(); i++)
+        cout << result2[i] << " ";
+    for (int i = 0; i < result3.size(); i++)
+        cout << result3[i] << " ";
 
     return 0;
 }
