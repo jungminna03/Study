3fa70bb434a59f09f1c479a46ca309aadfcc3d4b
jungminna03 <96573337+jungminna03@users.noreply.github.com>
2026-03-18 21:41:01 +0900

    [ADD] Search Insert Position
    Can't solved
    retry next time

---
+++ Algorithm/Esey/Search_Insert_Position.cpp
@@ -0,0 +1,28 @@
+﻿//
+// Created by jungk on 26. 3. 18..
+//
+
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int searchInsert(std::vector<int>& nums, int target) {
+        
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    vector<int> v = {1, 3, 5, 6};
+
+    auto r = s.searchInsert(v, 5);
+    auto r2 = s.searchInsert(v, 2);
+    auto r3 = s.searchInsert(v, 7);
+
+    return 0;
+}
