6c605ba17e75f2f780dbab87ed902a6ee847eade
jungminna03 <96573337+jungminna03@users.noreply.github.com>
2026-03-31 21:20:05 +0900

    [ADD] Intersection of Two Linked Lists

---
+++ Algorithm/Esey/Intersection_of_Two_Linked_Lists.cpp
@@ -0,0 +1,51 @@
+﻿//
+// Created by jungk on 26. 3. 31..
+//
+
+#include <cstddef>
+#include <unordered_map>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        while (headA != headB)
+        {
+            a = (a == nullptr) ? headB : a->next;
+            b = (b == nullptr) ? headA : b->next;
+        }
+
+        return a;
+    }
+};
+
+int main()
+{
+    ListNode *common = new ListNode(8);
+    common->next = new ListNode(4);
+    common->next->next = new ListNode(5);
+
+    ListNode *headA = new ListNode(4);
+    headA->next = new ListNode(1);
+    headA->next->next = common;
+
+    ListNode *headB = new ListNode(5);
+    headB->next = new ListNode(6);
+    headB->next->next = new ListNode(1);
+    headB->next->next->next = common;
+
+    Solution sol;
+    ListNode *result = sol.getIntersectionNode(headA, headB);
+
+    return 0;
+}
---
+++ Algorithm/Esey/Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -0,0 +1,37 @@
+﻿//
+// Created by jungk on 26. 3. 25..
+//
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        for (int i = 0; i < s.length(); i++)
+        {
+            for (int j = 0; j < s.length(); j++)
+            {
+                if (s[i] == s[j])
+                {
+                    s.erase(s.begin() + i);
+                    s.erase(s.begin() + j);
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    string s1 = "abbaca";
+    string s2 = "azxxzy";
+
+    auto res1 = s.removeDuplicates(s1);
+    auto res2 = s.removeDuplicates(s2);
+
+    return 0;
+}
