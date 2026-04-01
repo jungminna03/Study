c20ce6687dd85cf8e4a5f9dae99daa6edc6ff75d
jungminna03 <96573337+jungminna03@users.noreply.github.com>
2026-04-02 08:43:34 +0900

    [ADD] Reverse_Bits

---
+++ Algorithm/Esey/Reverse_Bits.cpp
@@ -0,0 +1,29 @@
+﻿//
+// Created by jungk on 26. 4. 2..
+//
+
+class Solution {
+public:
+    // 가장 "빠르게" 뒤집기 = 새로 배열 만들어서 뒤에서부터 집어넣기
+    int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; ++i)
+        {
+            res <<= 1;
+            res |= n & 1;
+            n >>= 1;
+        }
+
+        return res;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    auto result = s.reverseBits(43261596);
+    auto result2 = s.reverseBits(2147483644);
+
+    return 0;
+}
