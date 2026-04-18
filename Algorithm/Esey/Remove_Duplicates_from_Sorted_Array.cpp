f9eb544a16e9f58aa188afbbcd8d7bcce5a59282
jungminna03 <96573337+jungminna03@users.noreply.github.com>
2026-04-19 04:33:06 +0900

    [ADD] Remove_Duplicates_from_Sorted_Array

---
+++ Algorithm/Esey/Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,39 @@
+﻿//
+// Created by jungk on 26. 4. 19..
+//
+
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        int count = 0;
+
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            if (nums[i] != nums[count])
+            {
+                nums[++count] = nums[i];
+            }
+        }
+
+        return count + 1;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    vector<int> intput1 {1, 1, 2};
+    vector<int> intput2 {0, 0, 1, 1, 1, 2, 2, 3, 3 ,4};
+
+    auto res = s.removeDuplicates(intput1);
+    auto res2 = s.removeDuplicates(intput2);
+
+    return 0;
+}
