e87bf7fb03fa9f64677a9f26fe1b52ec51df804f
jungminna03 <96573337+jungminna03@users.noreply.github.com>
2026-04-22 21:12:47 +0900

    [ADD] First_Bad_Version

---
+++ Algorithm/Esey/First_Bad_Version.cpp
@@ -0,0 +1,51 @@
+﻿//
+// Created by jungk on 26. 4. 21..
+//
+
+class Solution {
+public:
+    int badVersion;
+
+    void setBadVersion(int version)
+    {
+        badVersion = version;
+    }
+
+    bool isBadVersion(int version)
+    {
+        return badVersion >= version;
+    }
+
+    int firstBadVersion(int n) {
+        if (n <= 1) return n;
+
+        int left = 1, right = n;
+        int mid;
+
+        while (left < right)
+        {
+            mid = (left + right) / 2;
+
+            if (left == mid)
+                return mid;
+
+                if (isBadVersion(mid)) left = mid;
+            else right = mid;
+        }
+
+        return left;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    s.setBadVersion(4);
+    auto res = s.firstBadVersion(5);
+
+    s.setBadVersion(1);
+    auto res2 = s.firstBadVersion(1);
+
+    return 0;
+}
