25a1edff67491877f27b0fa6083e9eabe8ccb7ac
jungminna03 <96573337+jungminna03@users.noreply.github.com>
2026-05-11 10:16:48 +0900

    [TEMP] Min_Stack

---
+++ Algorithm/Medium/Min_Stack.cpp
@@ -0,0 +1,44 @@
+﻿//
+// Created by jungk on 26. 5. 11.
+//
+
+#include <set>
+#include <stack>
+using namespace std;
+
+class MinStack {
+public:
+    stack<int> stk;
+
+    void push(int val) {
+        stk.push(val);
+
+    }
+
+    void pop() {
+
+        stk.pop();
+    }
+
+    int top() {
+        return stk.top();
+    }
+
+    int getMin() {
+        return 0;
+    }
+};
+
+int main()
+{
+    MinStack minStack;
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    auto res1 = minStack.getMin(); // return -3
+    minStack.pop();
+    auto res2 = minStack.top();    // return 0
+    auto res3 = minStack.getMin(); // return -2
+
+    return 0;
+}
